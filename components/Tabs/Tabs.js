f6ea37f324fa68163ef38dd4e655607c69b6c429
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-06-20 15:43:44 -0700

    updated to correct console error

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -10,6 +10,7 @@ class TabLink {
     this.itemElement = document.querySelector(
       `.tabs-item[data-tab="${this.data}"]`
     );
+
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
 
